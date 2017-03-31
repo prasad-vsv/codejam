a84c6943e8bab42e36771aad179ffc069a531e97
vsvprasad <vsvprasad108@gmail.com>
2017-03-31 15:16:42 +0530

    initial commit

---
+++ src/gcj2015/round1b/CounterCulture.java
@@ -0,0 +1,66 @@
+package round1b;
+import java.util.Scanner;
+
+
+public class CounterCulture {
+	static Scanner s = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int tests= Integer.parseInt(s.nextLine());
+		long[] input = new long[tests];
+		long[] output = new long[tests];
+		getInput(input);
+		for (int i=0;i<tests;++i){
+			output[i]  = getMinNum(input[i]);
+		}
+		prlongOutput(output);
+	}
+
+	private static void getInput(long[] input) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<input.length;i++){
+			input[i] = Long.parseLong(s.nextLine());
+		}
+	}
+
+	private static void prlongOutput(long[] output) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<output.length;++i){
+			System.out.println("Case #"+(i+1)+": "+ output[i]);
+		}
+	}
+
+	private static long getMinNum(long N) {
+		long count =0;
+		long start =1;
+		while(start<=N){
+			long reverse = getReverse(start+"");
+			if(reverse>(start+1) && reverse <=N){
+				start = reverse;
+				
+			}else{
+				start++;
+			}
+			count++;
+//			System.out.prlongln(count+":"+start);
+		}
+		
+		return count;
+	}
+
+	private static long getReverse(String start) {
+		if(start.substring(start.length()-1).equals("0")){
+			return 0;
+		}
+		StringBuilder temp= new StringBuilder(start);
+		
+		/*for(long i=start.length()-1; i>=0;i--){
+			temp.append(start.substring(i, i+1));
+		}*/
+		temp = temp.reverse();
+		
+		return Long.parseLong(temp.toString());
+	}
+
+}
